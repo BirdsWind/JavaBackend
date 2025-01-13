35ebad9ea1ef9c3b696fe3f38f5d10e86ec4a16a
Humlan <admin@humlans-MacBook-Pro.local>
2025-01-13 12:41:30 +0100

    add root endpoint

--- src/main/java/com/demo/controller/TaskController.java
+++ src/main/java/com/demo/controller/TaskController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.*;
 import java.io.IOException;
 
-
 import com.demo.service.TaskService;
 import com.demo.model.Task;
 
@@ -19,6 +18,11 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Java Backend API!";
+    }
+    
     @GetMapping("/tasks")
     public List<Task> getTasks() throws IOException {
         return taskService.loadTasksFromJson();
